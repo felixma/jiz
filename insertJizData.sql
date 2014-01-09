b38aa22085d06a1bd78d809f666df7c3fcb8992a
ubuntu13 <felix23ma@gmail.com>
2014-01-09 08:47:26 +0800

    changed some words into Chinese

--- insertJizData.sql
+++ insertJizData.sql
@@ -1,24 +1,27 @@
 use jiz;
 INSERT jizhang_customers VALUES 
-(123,'felix','song lin road','qingdao','shandong',266100,'China',18669851131,88615808,'felix@alu.com','taobao_felix','baidu_felix');
+(123,'felix','松岭路','青岛','山东',266100,'中国',18669851131,88615808,'felix@alu.com','taobao_felix','baidu_felix');
 INSERT jizhang_customers VALUES 
-(111,'felix','song lin road','qingdao','shandong',266100,'China',18669851131,88615808,'felix@alu.com','taobao_felix','baidu_felix');
+(111,'felix','松岭路','青岛','山东',266100,'中国',18669851131,88615808,'felix@alu.com','taobao_felix','baidu_felix');
 INSERT jizhang_customers VALUES 
-(222,'maqi','song lin road','qingdao','shandong',266100,'China',18669851131,88615808,'maqi@alu.com','taobao_maqi','baidu_maqi');
+(222,'maqi','松岭路','青岛','山东',266100,'中国',18669851131,88615808,'maqi@alu.com','taobao_maqi','baidu_maqi');
 
 INSERT jizhang_vendors VALUES
-(1,'Self','Si Ji Jing Yuan', 'QD','SD',266100, 'China');
+(1,'Self','四季景园', '青岛','山东',266100, '中国');
 INSERT jizhang_vendors VALUES
-(2,'vend_1','guangzhou street', 'GZ','GD',123456, 'China');
+(2,'vend_1','广州街', '广州','广东',123456, '中国');
 
 INSERT jizhang_products VALUES
-(1, 1, 'fa dai 001', 5, 50, 'green fa dai', 30, 1);
+(1, 1, '发带001', 5, 50, '绿色发带', 30, 1);
 INSERT jizhang_products VALUES
-(2, 1, 'fa dai 002', 20, 50, 'fa dai 002 yellow', 30, 1);
+(2, 1, '发带002', 20, 50, '发带002 浅黄色', 30, 1);
 INSERT jizhang_products VALUES
-(3, 1, 'fa dai 001', 5, 50, 'green fa dai', 30, 2);
+(3, 1, '发带001', 5, 50, '绿色发带', 30, 2);
 INSERT jizhang_products VALUES
-(4, 2, 'hua bu', 5, 20, 'hua bu - yellow', 300, 1);
+(4, 2, '花布1号', 5, 20, '花布 - yellow', 300, 1);
+INSERT jizhang_products VALUES
+(5, 2, '花布', 5, 20, '紫色花布', 300, 1);
+
 
 INSERT jizhang_orders VALUES
 (1, '2014/01/01', 111, 1, 'http://taobao.com');
@@ -49,6 +52,4 @@ INSERT jizhang_orderitems VALUES
 (6, 5, 4, 1, 20);
 INSERT jizhang_orderitems VALUES
 (7, 6, 3, 5, 20);
-
-
 
--- query.py
+++ query.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import MySQLdb
 conn = MySQLdb.connect(host='localhost', user='felix',passwd='hello',db='jiz')
 cursor = conn.cursor()
@@ -7,5 +8,6 @@ count = cursor.execute('select * from jizhang_products')
 results = cursor.fetchall()
 for r in results:
     print r
+    #print r[0].decode('utf8')
 cursor.close();
 conn.close()
